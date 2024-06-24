33382b8b135259bd0711ea40c322da0d99b3578a
Amir Kiadi <akiadimoghaddam@ucsd.edu>
2024-06-23 23:45:38 -0700

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -3,24 +3,22 @@ import './navigation.css'
 import imageOne from './resources2/logo.png';
 import {Link} from 'react-scroll';
 
-
-
 const NavigationBar = () => {
             return (
+                  
+
+
                     <nav className = "NavigationBar">
-                        <Link className = "homeButton">
-                            <img src = {imageOne} alt ="imageOne" className = "imageOne">
-                                </img>
-                                <text> Amir Kiadi </text>
-                            </Link>
+                        <div className = "picture">
+                        <img src = {imageOne} alt ="imageOne" className = "imageOne"></img>
+                        </div>
                         <div className = "menu">
+                                <Link className = "menuItemName">Amir Kiadi</Link>
                                 <Link className = "menuItem">About Me</Link>
                                 <Link className = "menuItem">Projects</Link>
                                 <Link className = "menuItem">Gallery</Link>
+                                <Link className = "menuItem">Contact</Link>
                         </div>
-                        <button className = "menuButton">
-                           Contact
-                        </button>
                     </nav>
             );
         }
