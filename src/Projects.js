585052599efc2a2c87008ed624d9473eb1f16f29
Amir <damianwritescode@gmail.com>
2025-08-29 18:24:59 -0700

    Forgot to add classname for image styling

--- src/Projects.js
+++ src/Projects.js
@@ -78,7 +78,7 @@ const Projects = () => {
                             to connect with classmates based on their interests and preferences.
                             Built using React.js, FASTAPI, JWT Authentication, and SQLAlchemy (PostgreSQL).
                         </p>
-                        <img src={imgThree} alt="Project Preview" className=""/>
+                        <img src={imgThree} alt="Project Preview" className="projImageTwo"/>
                         <button className="button" onClick={() => window.open('https://github.com/amirr-k/UCMe', '_blank')}>
                             <img src={git} alt="GitHub"/>
                         </button>
@@ -90,7 +90,7 @@ const Projects = () => {
                             starts/stops an EC2 instance from user speech with an 18s end to end response time.
                             Built using React.js, AWS Lambda, and AWS Transcribe.
                         </p>
-                        <img src={imgFour} alt="Project Preview" className=""/>
+                        <img src={imgFour} alt="Project Preview" className="projImageTwo"/>
                         <button className="button" onClick={() => window.open('https://github.com/amirr-k/Voice-Controlled-AWS', '_blank')}>
                             <img src={git} alt="GitHub"/>
                         </button>
